2218682bb19cad6d55e9ffc4628665e46a10a180
David Rivera Martinez <davirive@ucm.es>
2022-03-27 15:23:10 +0200

    cambiado program.cs estudiante b

--- Practica4MAP/Program.cs
+++ Practica4MAP/Program.cs
@@ -7,7 +7,7 @@ namespace Practica4MAP
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-			Console.WriteLine("Modificado por el Estudiante B");
+			Console.WriteLine("Estudiante B");
 		}
 	}
 }
