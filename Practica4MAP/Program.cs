25a84cec41f0a97ecce8a1402c92e985af74c629
DavidRainder <davirive@ucm.es>
2022-03-28 11:12:23 +0200

    Modificado Program.cs por estudiante B

--- Practica4MAP/Program.cs
+++ Practica4MAP/Program.cs
@@ -7,7 +7,7 @@ namespace Practica4MAP
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-			Console.WriteLine("Estudiante B");
+			Console.WriteLine("Cambio hecho por el Estudiante B");
 		}
 	}
 }
